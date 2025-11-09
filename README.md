07805bd9877d3d4f87afeca3c7c0e70c12612fb5
andydbui <buiand@fastmail.com>
2025-11-08 21:43:30 -0600

    fixing test case

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Roger Motorsports Library
 ## A 3D Asset Library
 
-// todo
+## Deploying to EC2
+
+### 1. SSH Into Production Environment
+
+### BUILD FRONTEND
+
+cd frontend
+npm run build
+
+### RUN FRONTEND
+
+
--- frontend/src/component/galleryComponent.test.tsx
+++ frontend/src/component/galleryComponent.test.tsx
@@ -34,6 +34,7 @@ const mockItemData: ItemType = {
     "standard": "Tsukuba Blue"
   },
   colors: ['red', 'blue'],
+  colorcodes: ['red', 'blue'],
   lods: ['high', 'medium'],
   lodmap: {
     "lod0": "LOD0",
@@ -43,11 +44,11 @@ const mockItemData: ItemType = {
     "lod0": "30634",
     "lod1": "4734"
   },
-  category: 'furniture',
-  subcategory: 'category1',
+  category: 'test',
+  subcategory: 'subcat_test',
   description: 'Test description',
   creatornote: 'Test note',
-  shader: 'standard',
+  shader: 'pbr',
   preview: 'https://example.com/preview.jpg',
   imagepath: '/images/',
   images: ['img1.jpg'],
@@ -61,6 +62,7 @@ const mockItemData: ItemType = {
             maps: ["false"]
         }
   ],
+  modelanimation: "false",
   texturemap: {
     "lod0": "0",
     "lod1": "0"
